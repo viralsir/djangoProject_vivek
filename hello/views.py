8fb4ea6932bf0f0771892fac6b3d519da2cde582
viralsir <viralsir2018@gmail.com>
2021-06-28 09:38:13 +0530

    render html page completed

--- hello/views.py
+++ hello/views.py
@@ -3,8 +3,11 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    #return HttpResponse("<h1>Home Page</h1>")
+    return render(request,"hello/home.html")
+
 def about(request):
-    return HttpResponse("<h1>About Page </h1>")
+    return render(request,"hello/about.html")
+
 def greetings(request,name):
     return HttpResponse("<h1> Hello "+name+"</h1>")
