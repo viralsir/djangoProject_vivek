942c398dee0e628ee96df2c93ad100c61589272b
viralsir <viralsir2018@gmail.com>
2021-07-05 10:02:18 +0530

    task app is created by form and save data into tasks list

--- taskdemo/url.py
+++ taskdemo/url.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import home,addtask
 urlpatterns=[
     path("list/",home,name="task-list"),
-    path("add/",addtask,name="task-add")
+    path("create/",addtask,name="task-add")
 ]
--- taskdemo/views.py
+++ taskdemo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django import forms
 
 # Create your views here.
 tasks=["check balance","recharge mobile",'visit showroom']
@@ -8,5 +9,22 @@ def home(request):
         "tasklist":tasks
     })
 
+class TaskForm(forms.Form):
+    task=forms.CharField(max_length=25)
+    priority=forms.IntegerField(min_value=1,max_value=7)
+
 def addtask(request):
-    return render(request,"taskdemo/addtask.html")
+    if request.method == 'POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            #request.POST["task"]
+            tasks.append(form.cleaned_data["task"])
+            return redirect('task-list')
+        else :
+            return render(request, "taskdemo/addtask.html", {
+                "form": form
+            })
+    form=TaskForm()
+    return render(request,"taskdemo/addtask.html",{
+        "form":form
+    })
