a254401e991c8389f2d73e0233f799ed278ad12c
viralsir <viralsir2018@gmail.com>
2021-07-14 10:31:22 +0530

    flight home and admin site is added

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import flight
 # Create your views here.
+def home(request):
+    no_of_flights=flight.objects.all()
+    return render(request,"flights/home.html",{
+        "no_of_flights":no_of_flights
+    })
