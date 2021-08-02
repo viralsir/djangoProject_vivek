e5b8c303ccad1d0ff71aade2c325882d2c9f33be
viralsir <viralsir2018@gmail.com>
2021-08-02 11:39:23 +0530

    detail page and new form is being created.

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import  path
-from emp_app.views import employee_list
+from emp_app.views import employee_list,new_employee,employee_detail
 urlpatterns=[
-    path("list/",employee_list,name="emp_list")
+    path("list/",employee_list,name="emp_list"),
+    path("new/",new_employee,name="emp-new"),
+    path("detail/<int:eid>",employee_detail,name="emp-detail")
 ]
--- emp_app/views.py
+++ emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from emp_app.models import employee
+from .models import employee
 # Create your views here.
 def employee_list(request):
     emplist=employee.objects.all()
@@ -8,3 +9,23 @@ def employee_list(request):
         "employee_list":emplist
 
     })
+
+def employee_detail(request,eid):
+    emp=employee.objects.get(id=eid)
+    return render(request,"emp_app/employee_detail.html",{
+        "emp":emp
+    })
+
+
+def new_employee(request):
+    if request.method == 'POST':
+        print(request.POST)
+        emp=employee()
+        emp.name=request.POST['name']
+        emp.address=request.POST['address']
+        emp.phno=request.POST['phno']
+        emp.salary=request.POST['salary']
+        emp.save()
+        return redirect('emp_list')
+    else:
+        return render(request,"emp_app/emp_form.html")
